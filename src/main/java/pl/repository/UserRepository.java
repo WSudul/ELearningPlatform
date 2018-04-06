612136e84d69b8f49e3385321f5564ad82b263b2
WSudul <wojciech.sudul@gmail.com>
2018-04-06 02:10:29 +0200

    index.html added + h2 db config & repostiory fixes

--- src/main/java/pl/repository/UserRepository.java
+++ src/main/java/pl/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package pl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.model.User;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
--- src/main/java/pl/repository/UserRoleRepository.java
+++ src/main/java/pl/repository/UserRoleRepository.java
@@ -1,13 +1,14 @@
 package pl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.model.UserRole;
 
 import java.util.Optional;
 
-
+@Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
     Optional<UserRole> findByRole(String role);
 
-    Optional<UserRole> findById(Long id);
+
 }
